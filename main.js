c30480dbde4793a2c01d371d12da9820575ba214
SergioZona <60414505+SergioZona@users.noreply.github.com>
2021-09-28 13:46:18 -0500

    Se agrega solución a la primera entrega

--- main.js
+++ main.js
@@ -1,71 +1,40 @@
 const url =
-  "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/";
+  "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
+
+//Punto 2. Dar click agrega.
+const but = document.getElementsByName("agregar");
+but.onclick = agregarItem;
+function agregarItem() {
+    let valor = document.getElementById("num-items").value;
+    console.log(valor);
+    document.getElementById("num-items").innerHTML = valor++;
+}
 
 fetch(url)
   .then((res) => res.json())
   .then((res) => {
-    
-    //Creación de variables.
-    let setEvents = new Set();
-    let cont = 0;
-
     // Obtención de los datos del JSON.
-    let table = document.getElementById("table-data");
+    let cards = document.getElementById("cards");
     res.forEach((element) => {
-      bgcolor = "";
-      if (element.squirrel) {
-        bgcolor = 'bgcolor = "f9c6cb"';
-      }
-      let row = `<tr ${bgcolor}>
-        <td>${(cont += 1)}</td>
-        <td class="event">${element.events}</td>
-        <td class="boolean">${element.squirrel}</td>
-        </tr>`;
-      table.innerHTML += row; //Se agregan los datos a la tabla.
-
-      // Se obtiene los eventos únicos y se agregan al Set.
-      values = element.events.toString().split(",");
-      values.forEach((element) => setEvents.add(element));
-    });
-
-    // Se generan nuevamente las variables.
-    setEvents = Array.from(setEvents);
-    cont=0;
-    
-    // Obtención de la correlación por evento.
-    let tuplas = {};
-    setEvents.forEach((event) => {
-      let [TP, TN, FP, FN] = [0, 0, 0, 0];      
-
-      res.forEach((element) => {
-        values = element.events.toString().split(",");
-        if(values.includes(event) && element.squirrel) {TP++}
-        if(!values.includes(event) && element.squirrel) {FP++}
-        if(values.includes(event) && !element.squirrel) {FN++}
-        if(!values.includes(event) && !element.squirrel) {TN++}      
+      element.products.forEach((product) => {
+        let card = `<div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${product.image}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text">${product.description}</p>
+            <p class="card-text">Precio: ${product.price}</p>
+            <a class="btn btn-primary" onclick="agregarItem()">Add To Car</a>
+          </div>
+        </div>
+        </div>`;
+        cards.innerHTML += card; //Se agregan los datos.
       });
 
-      let correlation = ((TP*TN)-(FP*FN))/(Math.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)));
-      tuplas[event] = correlation; // Se agrega la tupla de evento-correlación a un diccionario.
+      //Punto 2. Dar click agrega.
+      //const but = document.getElementsByName("agregar");
+      //but.onclick = agregarItem;
     });
-
-    // Ordenar las tuplas de eventos y correlación.
-    let items = Object.keys(tuplas).map(function(key) {
-        return [key, tuplas[key]];
-    });  
-    items.sort(function(first, second) {
-        return second[1] - first[1];
-    });
-    tuplas = items;
-    
-    // Se genera la segunda tabla.
-    for (const [key, value] of Object.entries(tuplas)) {
-        let table = document.getElementById("table-correlation");
-        let row = `<tr>
-                   <td>${(cont += 1)}</td>
-                   <td class="event">${value[0]}</td>
-                   <td class="correlation">${value[1]}</td>
-                   </tr>`;
-        table.innerHTML += row; //Se agregan los datos de evento y correlación a la tabla.
-    };
   });
+
+
